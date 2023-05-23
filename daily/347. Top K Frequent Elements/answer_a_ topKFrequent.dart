189f952df7124fa4536143da0499279961378d2b
GekiteniusY <genkichi.yogi@gmail.com>
2023-05-23 09:03:27 +0900

    Daily Challenge at May 22

---
+++ daily/347. Top K Frequent Elements/answer_a_ topKFrequent.dart
@@ -0,0 +1,23 @@
+List<int> topKFrequent(List<int> nums, int k) {
+  // Count the frequency of each number
+  final frequencyMap = <int, int>{};
+  for (final num in nums) {
+    frequencyMap[num] = (frequencyMap[num] ?? 0) + 1;
+  }
+
+  // Create a list of unique numbers
+  final uniqueNums = frequencyMap.keys.toList();
+
+  // Sort the unique numbers based on their frequencies
+  uniqueNums.sort((a, b) => frequencyMap[b]!.compareTo(frequencyMap[a]!));
+
+  // Extract the top k frequent elements
+  return uniqueNums.sublist(0, k);
+}
+
+void main() {
+  final nums = [1, 1, 1, 2, 2, 3];
+  final k = 2;
+  final topK = topKFrequent(nums, k);
+  print(topK); // Output: [1, 2]
+}
---
+++ daily/347. Top K Frequent Elements/answer_b_topKFrequent.dart
@@ -0,0 +1,42 @@
+// import 'dart:collection';
+
+// PriorityQueue is not define???
+// https://stackoverflow.com/questions/67818209/how-to-implement-priority-queue-in-dart
+// https://api.flutter.dev/flutter/package-collection_collection/PriorityQueue-class.html
+
+
+// List<int> topKFrequent(List<int> nums, int k) {
+//   // Count the frequency of each number
+//   final frequencyMap = <int, int>{};
+//   for (final num in nums) {
+//     frequencyMap[num] = (frequencyMap[num] ?? 0) + 1;
+//   }
+
+//   // Create a priority queue to store the top k frequent elements
+//   final queue = PriorityQueue<MapEntry<int, int>>(
+//     (a, b) => b.value.compareTo(a.value),
+//   );
+
+//   // Add elements to the priority queue
+//   for (final entry in frequencyMap.entries) {
+//     queue.add(entry);
+//     if (queue.length > k) {
+//       queue.removeLast();
+//     }
+//   }
+
+//   // Extract the top k frequent elements from the priority queue
+//   final result = <int>[];
+//   while (queue.isNotEmpty) {
+//     result.add(queue.removeFirst().key);
+//   }
+
+//   return result;
+// }
+
+// void main() {
+//   final nums = [1, 1, 1, 2, 2, 3];
+//   final k = 2;
+//   final topK = topKFrequent(nums, k);
+//   print(topK); // Output: [1, 2]
+// }
